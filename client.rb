06633ea7869f15611d76a298bc037f63dfe64849
geeton-dev <77248338+geeton-dev@users.noreply.github.com>
2021-01-27 12:01:25 +0000

    Create client.rb

---
+++ client.rb
@@ -0,0 +1,69 @@
+require 'httparty'
+
+FortniteUser = Struct.new(
+    :display_name,
+    :id,
+    :external_auths
+)
+
+class FortniteClient
+    def initialize(authorization_code=nil)
+        if authorization_code # Prompts an input for the exchange code if none is provided.
+            @_authorization_code = authorization_code
+        else
+            puts 'Please enter an exchange code:'
+            @_authorization_code = gets.chomp
+        end
+
+    end
+
+    def authenticate()
+        # Function to authenticate with the Epic Games API through exchange code.
+        request = HTTParty.post(
+            "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token",
+            :headers => {
+                "Content-Type" => "application/x-www-form-urlencoded",
+                "Authorization" => "basic ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ="
+            },
+            :body => {
+                "grant_type" => "authorization_code",
+                "exchange_code" => @_authorization_code
+            },
+            # :debug_output => $stdout
+        )
+
+        response = JSON.parse(request.body)
+
+        if request.code == 400
+            raise "#{response['errorCode']} - #{response['errorMessage']}"
+        end
+
+        @display_name = response['displayName']
+        @access_token = response['access_token']
+    end
+
+    def fetch_account(name)
+        # Fetch account information and returns a FortniteUser object.
+        request = HTTParty.get(
+            "https://account-public-service-prod03.ol.epicgames.com/account/api/public/account/displayName/#{name}",
+            :headers => {
+                "Authorization" => "bearer #{@access_token}"
+            })
+        
+
+        response = JSON.parse(request.body)
+        
+        FortniteUser.new(
+            response['displayName'],
+            response['id'],
+            response['externalAuths']
+        )
+    end
+
+    # Private attributes (not intended to be used outside of the object itself).
+    attr_accessor :_authorization_code
+
+    # Public attributes.
+    attr_accessor :display_name
+    attr_accessor :access_token
+end
